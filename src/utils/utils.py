43b7404205be4f3d62eb8d2a50c5a39eda9105f1
Masato Umakoshi <umakoshi@nlp.ist.i.kyoto-u.ac.jp>
2021-10-05 17:53:54 +0900

    Add logging

---
+++ src/utils/utils.py
@@ -0,0 +1,75 @@
+from typing import Sequence
+import logging
+import warnings
+
+import rich.syntax
+import rich.tree
+from omegaconf import DictConfig, OmegaConf
+
+
+def get_logger(name=__name__, level=logging.INFO) -> logging.Logger:
+    """Initializes multi-GPU-friendly python logger."""
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+
+    return logger
+
+
+def extras(config: DictConfig) -> None:
+    """A couple of optional utilities, controlled by main config file:
+    - disabling warnings
+    - easier access to debug mode
+    - forcing debug friendly configuration
+    Modifies DictConfig in place.
+    Args:
+        config (DictConfig): Configuration composed by Hydra.
+    """
+
+    log = get_logger()
+
+    # enable adding new keys to config
+    OmegaConf.set_struct(config, False)
+
+    # disable python warnings if <config.ignore_warnings=True>
+    if config.get("ignore_warnings"):
+        log.info("Disabling python warnings! <config.ignore_warnings=True>")
+        warnings.filterwarnings("ignore")
+
+    # if <config.name=...>
+    if config.get("name"):
+        log.info("Running in experiment mode! Name: {}".format(config.name))
+
+    # disable adding new keys to config
+    OmegaConf.set_struct(config, True)
+
+
+def print_config(
+    config: DictConfig,
+    fields: Sequence[str] = [],
+    resolve: bool = True,
+) -> None:
+    """Prints content of DictConfig using Rich library and its tree structure.
+    Args:
+        config (DictConfig): Configuration composed by Hydra.
+        fields (Sequence[str], optional): Determines which main fields from config will
+        be printed and in what order.
+        resolve (bool, optional): Whether to resolve reference fields of DictConfig.
+    """
+
+    tree = rich.tree.Tree("CONFIG")
+
+    for field in fields:
+        branch = tree.add(field)
+
+        config_section = config.get(field)
+        branch_content = str(config_section)
+        if isinstance(config_section, DictConfig):
+            branch_content = OmegaConf.to_yaml(config_section, resolve=resolve)
+
+        branch.add(rich.syntax.Syntax(branch_content, "yaml"))
+
+    rich.print(tree)
+
+    with open("config_tree.txt", "w") as fp:
+        rich.print(tree, file=fp)
